560b79408de46ec4c4c2088239a19c7f459adbfe
Danilo Valério da Silva <danilo_vs@hotmail.com>
2018-10-11 19:38:10 -0300

    Projeto finalizado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,15 @@
 package aplicacao;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Produto;
@@ -12,23 +17,45 @@ import entities.Produto;
 public class Programa {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Scanner sc = new Scanner(System.in);
-		String strCaminho = sc.nextLine();
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);		
+		List<Produto> lista = new ArrayList<>();
 		
-		String produtos[] = null;
-		ArrayList<Produto> lista = new ArrayList<Produto>();
+		System.out.print("Digite o caminho do arquivo> ");
+		String sourceFileStr = sc.nextLine();
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(strCaminho))) {
-			String linha = br.readLine();
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean sucesso = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\sumario.csv";
+				
+		try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+			String itemCsv = br.readLine();
 			
-			while (linha != null) {
-				System.out.println(linha);
-				produtos = linha.split(",");
-				linha = br.readLine();
-				lista.add(produtos[0],Double.parseDouble(produtos[1]),Integer.parseInt(produtos[2]));
+			while (itemCsv != null) {
+				String[] campos = itemCsv.split(",");
+				String nome = campos[0];
+				double preco = Double.parseDouble(campos[1]);
+				int quantidade = Integer.parseInt(campos[2]);
+				
+				lista.add(new Produto(nome, preco, quantidade));
+				
+				itemCsv = br.readLine();			
 			}
 			
-			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+				for (Produto item : lista) {
+					bw.write(item.getNome() + ","+ String.format("%.2f",  item.total()));
+					bw.newLine();
+				}
+				
+				System.out.println(targetFileStr + " CRIADO COM SUCESSO!");
+			} 
+			catch (IOException e) {
+				System.out.println("Erro ao criar arquivo: "+ e.getMessage());
+			}
 		}
 		catch (IOException e) {
 			System.out.println("Erro: "+ e.getMessage());
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -11,6 +11,44 @@ public class Produto {
 		this.quantidade = quantidade;
 	}
 	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public Double getPreco() {
+		return preco;
+	}
+
+
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+
 	public double total() {
 		return preco * quantidade;
 	}
